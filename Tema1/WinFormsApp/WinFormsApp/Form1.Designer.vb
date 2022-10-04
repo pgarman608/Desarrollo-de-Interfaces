452e343cea5df2c4e76314bfdcd8dccf5fcc2c32
Pablo <instypablo@gmail.com>
2022-10-04 21:33:59 +0200

    Aun falta por terminar

--- Tema1/WinFormsApp/WinFormsApp/Form1.Designer.vb
+++ Tema1/WinFormsApp/WinFormsApp/Form1.Designer.vb
@@ -22,10 +22,264 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.EjercicioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio3ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GBButtons = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.numtu = New System.Windows.Forms.MaskedTextBox()
+        Me.nu = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GBButtons.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EjercicioToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'EjercicioToolStripMenuItem
+        '
+        Me.EjercicioToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Ejercicio1ToolStripMenuItem, Me.Ejercicio2ToolStripMenuItem, Me.Ejercicio3ToolStripMenuItem})
+        Me.EjercicioToolStripMenuItem.Name = "EjercicioToolStripMenuItem"
+        Me.EjercicioToolStripMenuItem.Size = New System.Drawing.Size(68, 20)
+        Me.EjercicioToolStripMenuItem.Text = "Ejercicios"
+        '
+        'Ejercicio1ToolStripMenuItem
+        '
+        Me.Ejercicio1ToolStripMenuItem.Name = "Ejercicio1ToolStripMenuItem"
+        Me.Ejercicio1ToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.Ejercicio1ToolStripMenuItem.Text = "Ejercicio 1"
+        '
+        'Ejercicio2ToolStripMenuItem
+        '
+        Me.Ejercicio2ToolStripMenuItem.Name = "Ejercicio2ToolStripMenuItem"
+        Me.Ejercicio2ToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.Ejercicio2ToolStripMenuItem.Text = "Ejercicio 2"
+        '
+        'Ejercicio3ToolStripMenuItem
+        '
+        Me.Ejercicio3ToolStripMenuItem.Name = "Ejercicio3ToolStripMenuItem"
+        Me.Ejercicio3ToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.Ejercicio3ToolStripMenuItem.Text = "Ejercicio 3"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'GBButtons
+        '
+        Me.GBButtons.Controls.Add(Me.Button3)
+        Me.GBButtons.Controls.Add(Me.Button2)
+        Me.GBButtons.Controls.Add(Me.Button1)
+        Me.GBButtons.Location = New System.Drawing.Point(725, 396)
+        Me.GBButtons.Name = "GBButtons"
+        Me.GBButtons.Size = New System.Drawing.Size(800, 423)
+        Me.GBButtons.TabIndex = 1
+        Me.GBButtons.TabStop = False
+        Me.GBButtons.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(606, 195)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Ejercicio 3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(351, 195)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Ejercicio 2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(108, 195)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Ejercicio 1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox4)
+        Me.GroupBox1.Controls.Add(Me.MaskedTextBox3)
+        Me.GroupBox1.Controls.Add(Me.nu)
+        Me.GroupBox1.Controls.Add(Me.numtu)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(800, 425)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Calculo de necesidades combustibles"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(301, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Por favor, introduzcan estos datos"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(186, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Numero de turismos"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(186, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Numero de todoterrenos"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(186, 142)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(165, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Capacidad de turismos (litros)"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(186, 171)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(187, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Capacidad de todoterrenos (litros)"
+        '
+        'numtu
+        '
+        Me.numtu.Location = New System.Drawing.Point(483, 82)
+        Me.numtu.Name = "numtu"
+        Me.numtu.Size = New System.Drawing.Size(100, 23)
+        Me.numtu.TabIndex = 5
+        '
+        'nu
+        '
+        Me.nu.Location = New System.Drawing.Point(483, 110)
+        Me.nu.Name = "nu"
+        Me.nu.Size = New System.Drawing.Size(100, 23)
+        Me.nu.TabIndex = 6
+        '
+        'MaskedTextBox3
+        '
+        Me.MaskedTextBox3.Location = New System.Drawing.Point(483, 139)
+        Me.MaskedTextBox3.Name = "MaskedTextBox3"
+        Me.MaskedTextBox3.Size = New System.Drawing.Size(100, 23)
+        Me.MaskedTextBox3.TabIndex = 7
+        '
+        'MaskedTextBox4
+        '
+        Me.MaskedTextBox4.Location = New System.Drawing.Point(483, 168)
+        Me.MaskedTextBox4.Name = "MaskedTextBox4"
+        Me.MaskedTextBox4.Size = New System.Drawing.Size(100, 23)
+        Me.MaskedTextBox4.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(283, 209)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(10, 15)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "."
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(496, 209)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Aceptar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.GBButtons)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Ejercicio 3 (3en1): Labels, TexBoxs, Buttons, GroupBox, Menús"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GBButtons.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents EjercicioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio3ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GBButtons As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents MaskedTextBox4 As MaskedTextBox
+    Friend WithEvents MaskedTextBox3 As MaskedTextBox
+    Friend WithEvents nu As MaskedTextBox
+    Friend WithEvents numtu As MaskedTextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
