aabcf4a3f9200da55b56b31ff97dcac8f87e56fa
Pablo <instypablo@gmail.com>
2022-10-05 14:11:01 +0200

    Actividad Terminada v1

---
+++ Tema1/Ejercicio3/WinFormsApp/Form1.Designer.vb
@@ -0,0 +1,490 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.EjercicioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Ejercicio3ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GBButtons = New System.Windows.Forms.GroupBox()
+        Me.GBEjer1 = New System.Windows.Forms.GroupBox()
+        Me.GBEjer3 = New System.Windows.Forms.GroupBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.calcpremed = New System.Windows.Forms.Button()
+        Me.n3euros = New System.Windows.Forms.MaskedTextBox()
+        Me.n2euros = New System.Windows.Forms.MaskedTextBox()
+        Me.n1euros = New System.Windows.Forms.MaskedTextBox()
+        Me.fin3 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GBCilindro = New System.Windows.Forms.GroupBox()
+        Me.cilAlt = New System.Windows.Forms.MaskedTextBox()
+        Me.calcVol = New System.Windows.Forms.Button()
+        Me.cilDia = New System.Windows.Forms.MaskedTextBox()
+        Me.fin2 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btAceptar1 = New System.Windows.Forms.Button()
+        Me.fin1 = New System.Windows.Forms.Label()
+        Me.litrosTO = New System.Windows.Forms.MaskedTextBox()
+        Me.litrosTu = New System.Windows.Forms.MaskedTextBox()
+        Me.numTo = New System.Windows.Forms.MaskedTextBox()
+        Me.numtu = New System.Windows.Forms.MaskedTextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GBButtons.SuspendLayout()
+        Me.GBEjer1.SuspendLayout()
+        Me.GBEjer3.SuspendLayout()
+        Me.GBCilindro.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EjercicioToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'EjercicioToolStripMenuItem
+        '
+        Me.EjercicioToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Ejercicio1ToolStripMenuItem, Me.Ejercicio2ToolStripMenuItem, Me.Ejercicio3ToolStripMenuItem})
+        Me.EjercicioToolStripMenuItem.Name = "EjercicioToolStripMenuItem"
+        Me.EjercicioToolStripMenuItem.Size = New System.Drawing.Size(68, 20)
+        Me.EjercicioToolStripMenuItem.Text = "Ejercicios"
+        '
+        'Ejercicio1ToolStripMenuItem
+        '
+        Me.Ejercicio1ToolStripMenuItem.Name = "Ejercicio1ToolStripMenuItem"
+        Me.Ejercicio1ToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.Ejercicio1ToolStripMenuItem.Text = "Ejercicio 1"
+        '
+        'Ejercicio2ToolStripMenuItem
+        '
+        Me.Ejercicio2ToolStripMenuItem.Name = "Ejercicio2ToolStripMenuItem"
+        Me.Ejercicio2ToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.Ejercicio2ToolStripMenuItem.Text = "Ejercicio 2"
+        '
+        'Ejercicio3ToolStripMenuItem
+        '
+        Me.Ejercicio3ToolStripMenuItem.Name = "Ejercicio3ToolStripMenuItem"
+        Me.Ejercicio3ToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.Ejercicio3ToolStripMenuItem.Text = "Ejercicio 3"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'GBButtons
+        '
+        Me.GBButtons.Controls.Add(Me.GBEjer1)
+        Me.GBButtons.Controls.Add(Me.Button3)
+        Me.GBButtons.Controls.Add(Me.Button2)
+        Me.GBButtons.Controls.Add(Me.Button1)
+        Me.GBButtons.Location = New System.Drawing.Point(0, 27)
+        Me.GBButtons.Name = "GBButtons"
+        Me.GBButtons.Size = New System.Drawing.Size(800, 429)
+        Me.GBButtons.TabIndex = 1
+        Me.GBButtons.TabStop = False
+        '
+        'GBEjer1
+        '
+        Me.GBEjer1.Controls.Add(Me.GBEjer3)
+        Me.GBEjer1.Controls.Add(Me.btAceptar1)
+        Me.GBEjer1.Controls.Add(Me.fin1)
+        Me.GBEjer1.Controls.Add(Me.litrosTO)
+        Me.GBEjer1.Controls.Add(Me.litrosTu)
+        Me.GBEjer1.Controls.Add(Me.numTo)
+        Me.GBEjer1.Controls.Add(Me.numtu)
+        Me.GBEjer1.Controls.Add(Me.Label5)
+        Me.GBEjer1.Controls.Add(Me.Label4)
+        Me.GBEjer1.Controls.Add(Me.Label3)
+        Me.GBEjer1.Controls.Add(Me.Label2)
+        Me.GBEjer1.Controls.Add(Me.Label1)
+        Me.GBEjer1.Location = New System.Drawing.Point(0, 4)
+        Me.GBEjer1.Name = "GBEjer1"
+        Me.GBEjer1.Size = New System.Drawing.Size(800, 425)
+        Me.GBEjer1.TabIndex = 2
+        Me.GBEjer1.TabStop = False
+        Me.GBEjer1.Text = "Calculo de necesidades combustibles"
+        Me.GBEjer1.Visible = False
+        '
+        'GBEjer3
+        '
+        Me.GBEjer3.Controls.Add(Me.Label10)
+        Me.GBEjer3.Controls.Add(Me.Label9)
+        Me.GBEjer3.Controls.Add(Me.calcpremed)
+        Me.GBEjer3.Controls.Add(Me.n3euros)
+        Me.GBEjer3.Controls.Add(Me.n2euros)
+        Me.GBEjer3.Controls.Add(Me.n1euros)
+        Me.GBEjer3.Controls.Add(Me.fin3)
+        Me.GBEjer3.Controls.Add(Me.Label8)
+        Me.GBEjer3.Controls.Add(Me.GBCilindro)
+        Me.GBEjer3.Location = New System.Drawing.Point(0, 1)
+        Me.GBEjer3.Name = "GBEjer3"
+        Me.GBEjer3.Size = New System.Drawing.Size(800, 424)
+        Me.GBEjer3.TabIndex = 2
+        Me.GBEjer3.TabStop = False
+        Me.GBEjer3.Text = "Cálculo precio medio producto"
+        Me.GBEjer3.Visible = False
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(62, 106)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(312, 15)
+        Me.Label10.TabIndex = 8
+        Me.Label10.Text = "Introduzca aquí el precio en establecimiento nº3, en euros"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(62, 74)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(312, 15)
+        Me.Label9.TabIndex = 3
+        Me.Label9.Text = "Introduzca aquí el precio en establecimiento nº2, en euros"
+        '
+        'calcpremed
+        '
+        Me.calcpremed.Location = New System.Drawing.Point(241, 146)
+        Me.calcpremed.Name = "calcpremed"
+        Me.calcpremed.Size = New System.Drawing.Size(133, 23)
+        Me.calcpremed.TabIndex = 7
+        Me.calcpremed.Text = "Calcular precio medio"
+        Me.calcpremed.UseVisualStyleBackColor = True
+        '
+        'n3euros
+        '
+        Me.n3euros.Location = New System.Drawing.Point(380, 103)
+        Me.n3euros.Mask = "99999"
+        Me.n3euros.Name = "n3euros"
+        Me.n3euros.Size = New System.Drawing.Size(100, 23)
+        Me.n3euros.TabIndex = 6
+        Me.n3euros.ValidatingType = GetType(Integer)
+        '
+        'n2euros
+        '
+        Me.n2euros.Location = New System.Drawing.Point(380, 74)
+        Me.n2euros.Mask = "99999"
+        Me.n2euros.Name = "n2euros"
+        Me.n2euros.Size = New System.Drawing.Size(100, 23)
+        Me.n2euros.TabIndex = 5
+        Me.n2euros.ValidatingType = GetType(Integer)
+        '
+        'n1euros
+        '
+        Me.n1euros.Location = New System.Drawing.Point(380, 45)
+        Me.n1euros.Mask = "99999"
+        Me.n1euros.Name = "n1euros"
+        Me.n1euros.Size = New System.Drawing.Size(100, 23)
+        Me.n1euros.TabIndex = 4
+        Me.n1euros.ValidatingType = GetType(Integer)
+        '
+        'fin3
+        '
+        Me.fin3.AutoSize = True
+        Me.fin3.Location = New System.Drawing.Point(186, 194)
+        Me.fin3.Name = "fin3"
+        Me.fin3.Size = New System.Drawing.Size(0, 15)
+        Me.fin3.TabIndex = 2
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(62, 45)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(312, 15)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Introduzca aquí el precio en establecimiento nº1, en euros"
+        '
+        'GBCilindro
+        '
+        Me.GBCilindro.Controls.Add(Me.cilAlt)
+        Me.GBCilindro.Controls.Add(Me.calcVol)
+        Me.GBCilindro.Controls.Add(Me.cilDia)
+        Me.GBCilindro.Controls.Add(Me.fin2)
+        Me.GBCilindro.Controls.Add(Me.Label7)
+        Me.GBCilindro.Controls.Add(Me.Label6)
+        Me.GBCilindro.Location = New System.Drawing.Point(0, 0)
+        Me.GBCilindro.Name = "GBCilindro"
+        Me.GBCilindro.Size = New System.Drawing.Size(800, 425)
+        Me.GBCilindro.TabIndex = 2
+        Me.GBCilindro.TabStop = False
+        Me.GBCilindro.Text = "Calcular volumen cilindro"
+        Me.GBCilindro.Visible = False
+        '
+        'cilAlt
+        '
+        Me.cilAlt.Location = New System.Drawing.Point(387, 81)
+        Me.cilAlt.Mask = "99999"
+        Me.cilAlt.Name = "cilAlt"
+        Me.cilAlt.Size = New System.Drawing.Size(100, 23)
+        Me.cilAlt.TabIndex = 5
+        Me.cilAlt.ValidatingType = GetType(Integer)
+        '
+        'calcVol
+        '
+        Me.calcVol.Location = New System.Drawing.Point(299, 132)
+        Me.calcVol.Name = "calcVol"
+        Me.calcVol.Size = New System.Drawing.Size(117, 23)
+        Me.calcVol.TabIndex = 4
+        Me.calcVol.Text = "Calcualar Volumen"
+        Me.calcVol.UseVisualStyleBackColor = True
+        '
+        'cilDia
+        '
+        Me.cilDia.Location = New System.Drawing.Point(387, 35)
+        Me.cilDia.Mask = "99999"
+        Me.cilDia.Name = "cilDia"
+        Me.cilDia.Size = New System.Drawing.Size(100, 23)
+        Me.cilDia.TabIndex = 3
+        Me.cilDia.ValidatingType = GetType(Integer)
+        '
+        'fin2
+        '
+        Me.fin2.AutoSize = True
+        Me.fin2.Location = New System.Drawing.Point(241, 185)
+        Me.fin2.Name = "fin2"
+        Me.fin2.Size = New System.Drawing.Size(0, 15)
+        Me.fin2.TabIndex = 2
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(175, 84)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(162, 15)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "Introduce la altura, en metros"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(163, 38)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(174, 15)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Introduce el diamtro, en metros"
+        '
+        'btAceptar1
+        '
+        Me.btAceptar1.Location = New System.Drawing.Point(496, 209)
+        Me.btAceptar1.Name = "btAceptar1"
+        Me.btAceptar1.Size = New System.Drawing.Size(75, 23)
+        Me.btAceptar1.TabIndex = 10
+        Me.btAceptar1.Text = "Aceptar"
+        Me.btAceptar1.UseVisualStyleBackColor = True
+        '
+        'fin1
+        '
+        Me.fin1.AutoSize = True
+        Me.fin1.Location = New System.Drawing.Point(239, 245)
+        Me.fin1.Name = "fin1"
+        Me.fin1.Size = New System.Drawing.Size(0, 15)
+        Me.fin1.TabIndex = 9
+        '
+        'litrosTO
+        '
+        Me.litrosTO.Location = New System.Drawing.Point(483, 168)
+        Me.litrosTO.Mask = "99999"
+        Me.litrosTO.Name = "litrosTO"
+        Me.litrosTO.Size = New System.Drawing.Size(100, 23)
+        Me.litrosTO.TabIndex = 8
+        Me.litrosTO.ValidatingType = GetType(Integer)
+        '
+        'litrosTu
+        '
+        Me.litrosTu.Location = New System.Drawing.Point(483, 139)
+        Me.litrosTu.Mask = "99999"
+        Me.litrosTu.Name = "litrosTu"
+        Me.litrosTu.Size = New System.Drawing.Size(100, 23)
+        Me.litrosTu.TabIndex = 7
+        Me.litrosTu.ValidatingType = GetType(Integer)
+        '
+        'numTo
+        '
+        Me.numTo.Location = New System.Drawing.Point(483, 110)
+        Me.numTo.Mask = "99999"
+        Me.numTo.Name = "numTo"
+        Me.numTo.Size = New System.Drawing.Size(100, 23)
+        Me.numTo.TabIndex = 6
+        Me.numTo.ValidatingType = GetType(Integer)
+        '
+        'numtu
+        '
+        Me.numtu.Location = New System.Drawing.Point(483, 82)
+        Me.numtu.Mask = "99999"
+        Me.numtu.Name = "numtu"
+        Me.numtu.Size = New System.Drawing.Size(100, 23)
+        Me.numtu.TabIndex = 5
+        Me.numtu.ValidatingType = GetType(Integer)
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(186, 171)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(187, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Capacidad de todoterrenos (litros)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(186, 142)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(165, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Capacidad de turismos (litros)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(186, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Numero de todoterrenos"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(186, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Numero de turismos"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(301, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(186, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Por favor, introduzcan estos datos"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(606, 195)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Ejercicio 3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(391, 195)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Ejercicio 2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(139, 195)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Ejercicio 1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.GBButtons)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Ejercicio 3 (3en1): Labels, TexBoxs, Buttons, GroupBox, Menús"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GBButtons.ResumeLayout(False)
+        Me.GBEjer1.ResumeLayout(False)
+        Me.GBEjer1.PerformLayout()
+        Me.GBEjer3.ResumeLayout(False)
+        Me.GBEjer3.PerformLayout()
+        Me.GBCilindro.ResumeLayout(False)
+        Me.GBCilindro.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents EjercicioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Ejercicio3ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GBButtons As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GBEjer1 As GroupBox
+    Friend WithEvents btAceptar1 As Button
+    Friend WithEvents fin1 As Label
+    Friend WithEvents litrosTO As MaskedTextBox
+    Friend WithEvents litrosTu As MaskedTextBox
+    Friend WithEvents numTo As MaskedTextBox
+    Friend WithEvents numtu As MaskedTextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GBCilindro As GroupBox
+    Friend WithEvents calcVol As Button
+    Friend WithEvents cilDia As MaskedTextBox
+    Friend WithEvents fin2 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents cilAlt As MaskedTextBox
+    Friend WithEvents GBEjer3 As GroupBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents calcpremed As Button
+    Friend WithEvents n3euros As MaskedTextBox
+    Friend WithEvents n2euros As MaskedTextBox
+    Friend WithEvents n1euros As MaskedTextBox
+    Friend WithEvents fin3 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Tema1/Ejercicio3/WinFormsApp/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Friend Sub moverEjercicio1()
+        If GBButtons.Visible = True Then
+            GBButtons.Visible = False
+            GBEjer1.Visible = True
+        ElseIf GBCilindro.Visible = True Then
+            GBCilindro.Visible = False
+            GBEjer1.Visible = True
+        ElseIf GBEjer3.Visible = True Then
+            GBEjer3.Visible = False
+            GBEjer1.Visible = True
+        End If
+    End Sub
+    Friend Sub moverEjercicio2()
+        If GBButtons.Visible = True Then
+            GBButtons.Visible = False
+            GBCilindro.Visible = True
+        ElseIf GBEjer1.Visible = True Then
+            GBEjer1.Visible = False
+            GBCilindro.Visible = True
+        ElseIf GBEjer3.Visible = True Then
+            GBEjer3.Visible = False
+            GBCilindro.Visible = True
+        End If
+    End Sub
+    Friend Sub moverEjercicio3()
+        If GBButtons.Visible = True Then
+            GBButtons.Visible = False
+            GBEjer3.Visible = True
+        ElseIf GBEjer1.Visible = True Then
+            GBEjer1.Visible = False
+            GBEjer3.Visible = True
+        ElseIf GBCilindro.Visible = True Then
+            GBCilindro.Visible = False
+            GBEjer3.Visible = True
+        End If
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Application.ExitThread()
+    End Sub
+
+    Private Sub Ejercicio1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Ejercicio1ToolStripMenuItem.Click
+        moverEjercicio1()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        moverEjercicio1()
+    End Sub
+    Dim Solucion As Int32
+    Private Sub btAceptar1_Click(sender As Object, e As EventArgs) Handles btAceptar1.Click
+        Solucion = (numtu.Text * litrosTu.Text) + (litrosTO.Text * numTo.Text)
+        fin1.Text = "Las necesidades totales del combustible son de " & Solucion & " litros"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GBEjer1.Parent = Me
+        GBButtons.Parent = Me
+        GBCilindro.Parent = Me
+        GBEjer3.Parent = Me
+    End Sub
+
+    Private Sub Ejercicio2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Ejercicio2ToolStripMenuItem.Click
+        moverEjercicio2()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        moverEjercicio2()
+    End Sub
+    Dim pi As Int32 = 3.141593
+    Private Sub calcVol_Click(sender As Object, e As EventArgs) Handles calcVol.Click
+        Dim radio = cilDia.Text / 2
+        Dim calc = pi * (radio ^ 2) * cilAlt.Text
+        fin2.Text = "El volumen del cilindro es de " & calc & " metros cubicos"
+    End Sub
+
+    Private Sub Ejercicio3ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Ejercicio3ToolStripMenuItem.Click
+        moverEjercicio3()
+    End Sub
+
+    Private Sub calcpremed_Click(sender As Object, e As EventArgs) Handles calcpremed.Click
+        Dim calc = (Integer.Parse(n1euros.Text) + Integer.Parse(n2euros.Text) + Integer.Parse(n3euros.Text)) / 3
+        fin3.Text = "El precio medio del producto es de " & calc & " euros"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        moverEjercicio3()
+    End Sub
+
+End Class
